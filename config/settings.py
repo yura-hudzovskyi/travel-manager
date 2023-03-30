209d42750051cba3009beb084568dec339e9b1bf
Yura Hudzovskiy <50211819+yura-hudzovskyi@users.noreply.github.com>
2023-03-30 19:17:22 +0300

    rename DJANGO_SECRET_KEY -> SECRET_KEY and add new host to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-DJANGO_SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "travel-manager.onrender.com"]
 
 
 # Application definition
