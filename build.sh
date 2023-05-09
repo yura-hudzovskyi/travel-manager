cdd70e6646ffd9f1e09985bd3cb6157f949f094c
Yura Hudzovskiy <50211819+yura-hudzovskyi@users.noreply.github.com>
2023-05-09 13:23:05 +0300

    fixes

--- build.sh
+++ build.sh
@@ -2,5 +2,8 @@
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
-python manage.py migrate
+python3.9 -m pip install -r requirements.txt
+python3.9 -m manage.py collectstatic --noinput
+
+python3.9 -m manage.py makemigrations
+python3.9 -m manage.py migrate
